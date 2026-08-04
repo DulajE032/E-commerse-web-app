6e9116e4e32a7d39b06266155e88d1e138b8be22
DulajE032 <e22032@eng.pdn.ac.lk>
2026-08-05 01:24:49 +0530

    allowed credintial

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,6 +18,7 @@ app = FastAPI(title=settings.APP_NAME)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.CORS_ORIGINS,
+allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
